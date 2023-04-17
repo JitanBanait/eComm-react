0c8d18d76d1eb581786985f432144d866bcde9f8
jatin <sainijatin0399@gmail.com>
2023-04-17 21:57:12 +0530

    react

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -36,61 +36,62 @@ const Home = () => {
   
      return ( 
             <>
-                {typeof(prData) === 'undefined' ? <h1>Loading</h1>:
-                <>
+                {
+                    typeof(prData) === 'undefined' ? <h1>Loading</h1>:
+                 <>
 
-            <Nav userName={isUser.user} userImg={isUser.image}/>
-            <div>Home Page</div>  
-            
-                     <CardGroup  className="col-30 h-30 w-30 mb-2 gap-4">
-            {
-                prData.map(function(item,index){
-            return <div id={`main${item.id}`} key={main${item._id}}>
+                    <Nav userName={isUser.user} userImg={isUser.image}/>
+                    <div>Home Page</div>  
+                    
+                            <CardGroup  className="col-30 h-30 w-30 mb-2 gap-4">
+                    {
+                        prData.map(function(item,index){
+                    return <div id={`main${item.id}`} key={main${item._id}}>
 
-            <Card>
-                <Card.Img variant="top" src={`${config.BaseAPI}/${item.image}`} />
-                <Card.Body>
-                <Card.Title>{item.productName}</Card.Title>
-                <Card.Text>
-                   <p>Price:{item.productPrice}</p>
-                </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <Button variant="primary" onClick={handleShow(item.id)}>
-                Description
-            </Button>
-                    <Button id={`cartBtn_${item.id}`} >Add to cart</Button>
-                </Card.Footer>
-            </Card>
-            {/*--------modal----------- */ }
-             <Modal show={show} onHide={handleClose(item.id)}>
-                <Modal.Header closeButton>
-                <Modal.Title>{item.productName}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>{item.prDescription}</Modal.Body>
-                <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose(item.id)}>
-                    Close
-                </Button>
-                <Button variant="primary" onClick={handleClose(k)}>
-                    Save Changes
-                </Button>
-                </Modal.Footer>
-            </Modal>
-                </div>
+                    <Card>
+                        <Card.Img variant="top" src={`${config.BaseAPI}/${item.image}`} />
+                        <Card.Body>
+                        <Card.Title>{item.productName}</Card.Title>
+                        <Card.Text>
+                        <p>Price:{item.productPrice}</p>
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <Button variant="primary" onClick={handleShow(item.id)}>
+                        Description
+                    </Button>
+                            <Button id={`cartBtn_${item.id}`} >Add to cart</Button>
+                        </Card.Footer>
+                    </Card>
+                    {/*--------modal----------- */ }
+                    <Modal show={show} onHide={handleClose(item.id)}>
+                        <Modal.Header closeButton>
+                        <Modal.Title>{item.productName}</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>{item.prDescription}</Modal.Body>
+                        <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose(item.id)}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={handleClose(k)}>
+                            Save Changes
+                        </Button>
+                        </Modal.Footer>
+                    </Modal>
+                        </div>
 
-            })
-            }
-            
-            </CardGroup> 
-                
-                    {/* <div>
-                        {prData.map((val)=>{
-                            return <h5>{val.productName}</h5>
-                        })}
-                    </div> */}
-                }
- 
+                    })
+                    }
+                    
+                    </CardGroup> 
+                        
+                            {/* <div>
+                                {prData.map((val)=>{
+                                    return <h5>{val.productName}</h5>
+                                })}
+                            </div> */}
+                        }
+        
 
 
 
